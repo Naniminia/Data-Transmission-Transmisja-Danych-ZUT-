ab9fa0fb60617e8079f8601f2c0fd3fe75a6c94a
Karolina Piwoni <pk53787>
2024-04-15 17:25:58 +0200

    lab-2

--- lab-2/zadanie2/kod.py
+++ lab-2/zadanie2/kod.py
@@ -2,37 +2,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
+from time import time
+import cmath
+import math
+
 
 Tc = 3.0
-fs = 10000
+fs = 100
 fi = 180
 f = 1000
-N = int(Tc * fs)
+N = math.floor(Tc * fs)
 
 x = np.linspace(0, Tc, N)
+y = []
 
-def dft(x):
-    N = len(x)
-    n=np.arange(N)
-    k = n.reshape((N,1))
-    e = np.exp(-2j*np.pi*k*n/N)
-    return np.dot(e, x)
+def dft(y, N):
+    wyjscie =[]
+    for k in range(int(N/2)):
+        wynik = 0;
+        for n in range(N):
+            wynik += y[n]*cmath.exp((-1j*2*math.pi*k*n)/N)
+        wyjscie.append(wynik)
+    return wyjscie
 
 #plt.plot(dft(np.sin(2*np.pi*f*x)))
 #plt.show()
 
 #zad 2
-def M(x):
-    M = np.sqrt((np.real(dft(x)))**2+(np.imag(x))**2)
-    return M
+def M(y, N):
+    przyklad = dft(y, N)
+    wyjscie1 = []
+    for k in range(int(N / 2)):
+        wyjscie1.append(np.sqrt((np.real(przyklad)) ** 2 + (np.imag(przyklad)) ** 2))
+    return wyjscie1
 
-def Mprim(x):
-    M1 = 10*log10(M(x))
-    return M1
+def Mprim(y, N):
+    przyklad2 = M(y, N)
+    wyjscie2 = []
+    for k in range(int(N/2)):
+        wyjscie2 = 10*math.log10(przyklad2)
+    return wyjscie2
 
-fk = x*fs/N
+def skala(y, N):
+    wyjscie3 = []
+    for k in range(int(N/2)):
+        wyjscie3 = x*fs/N
+    return wyjscie3
 
-#zmienić skalę
-plt.plot(M(np.sin(2*np.pi*f*x)))
+def wywolanie(Tc, fs, N, f):
+    x1 = []
+    y = []
+    N = int(Tc*fs)
+    for t in range(0, N):
+        t=N/fs
+        x.append(t)
+        w=dft(t)
+        y.append(w)
+    return x1, y
+
+skala1 = skala(fs, N)
+dana = M(fs, N)
+
+plt.plot(skala1, dana)
 plt.show()
 plt.savefig("widmo")
