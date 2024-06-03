1ea8617d14e22b1ce09439b403007e2ace5c810e
Karolina Piwoni <pk53787>
2024-06-03 12:49:17 +0200

    lab-6

--- lab-6/kod.py
+++ lab-6/kod.py
@@ -52,31 +52,45 @@ def zad1():
     print(b1)
     print(S1)
 
-#zad1()
+zad1()
 
-# #zad2
-#   Macierz = np.array([
-#     [0, 0, 0, 1],
-#     [0, 0, 1, 0],
-#     [0, 0, 1, 1],
-#     [0, 1, 0, 0],
-#     [0, 1, 0, 1],
-#     [0, 1, 1, 0],
-#     [0, 1, 1, 1],
-#     [1, 0, 0, 0],
-#     [1, 0, 0, 1],
-#     [1, 0, 1, 0],
-#     [1, 0, 1, 1],
-#     [1, 1, 0, 0],
-#     [1, 1, 0, 1],
-#     [1, 1, 1, 0],
-#     [1, 1, 1, 1]
-#   ])
+#zad2
+Macierz = np.array([
+    [0, 0, 0, 1],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 1, 0, 0],
+    [0, 1, 0, 1],
+        [0, 1, 1, 0],
+        [0, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1],
+        [1, 0, 1, 0],
+        [1, 0, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 0, 1],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ])
 
-def HammingKoder15(wejscie):
-    wejscie =np.array()
-    Ik = np.eye(11, int)
-    P = np.array([
+bp = [1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1] #b początkowe
+# b[0] = bp[0]
+# b[1] = bp[1]
+# b[2] = bp[3]
+# b[3] = bp[7]
+# b[4] = bp[2]
+# b[5] = bp[5]
+# b[6] = bp[6]
+# b[7] = bp[7]
+# b[8] = bp[8]
+# b[9] = bp[9]
+# b[10] = bp[10]
+# b[11] = bp[11]
+# b[12] = bp[12]
+# b[13] = bp[13]
+# b[14] = bp[14]
+
+P = np.array([
         [1, 1, 0, 0],
         [1, 0, 1, 0],
         [0, 1, 1, 0],
@@ -89,7 +103,34 @@ def HammingKoder15(wejscie):
         [0, 1, 1, 1],
         [1, 1, 1, 1]
     ])
-    G = np.hstack(P, Ik)
-    c = wejscie*G
+
+def HammingKoder1511(wejscie):
+
+    Ik = np.eye(11, dtype = int)
+
+    G = np.hstack((P, Ik))
+    c = np.dot(wejscie, G)
+    c = c%2
     return c
 
+def HammingDekoder1511(wejscie1):
+    Pt = np.transpose(P)
+    Ink = np.eye(4, dtype = int)
+    H = np.hstack((Ink, Pt))
+    Ht = np.transpose(H)
+    s = np.dot(wejscie1, Ht)
+    s = s%2
+    SumaKontrolna = ((s[0]* pow(2, 0)) + (s[1] * pow(2, 1)) + (s[2] * pow(2, 2)) + s[3]*pow(2,3))
+    return s, SumaKontrolna
+
+# z = HammingKoder1511(bp)
+# print(z)
+# a, suma = HammingDekoder1511(z)
+# print(suma)
+# print(a)
+# print("ze zmianą")
+# z[4] = not z[4]
+# print(z)
+# a1, suma1 = HammingDekoder1511(z)
+# print(suma1)
+# print(a1)
